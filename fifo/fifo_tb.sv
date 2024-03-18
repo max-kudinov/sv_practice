078abb02a244d7fb8322fe9156b1f5eeb2ed70e1
mkudinov <m.kudinov@icloud.com>
2024-03-18 19:00:11 +0300

    fix testbench for fifo

--- fifo/fifo_tb.sv
+++ fifo/fifo_tb.sv
@@ -1,23 +1,27 @@
 `timescale 1ns/100ps
 
 module fifo_tb();
-    localparam CLK_PERIOD = 10;
-    localparam W_FIFO     = 8;
-    localparam D_FIFO     = 8;
-    localparam DELAY_MIN  = 0;
-    localparam DELAY_MAX  = 100;
-    localparam CHECKS     = 1000;
+    localparam CLK_PERIOD     = 10;
+    localparam W_FIFO         = 8;
+    localparam D_FIFO         = 8;
+    localparam PUSH_DELAY_MIN = 0;
+    localparam PUSH_DELAY_MAX = 10;
+    localparam POP_DELAY_MIN  = 0;
+    localparam POP_DELAY_MAX  = 10;
+    localparam CHECKS         = 1000;
 
-    logic clk, rst;
-
+    logic                clk;
+    logic                rst;
     logic [W_FIFO - 1:0] up_data;
     logic [W_FIFO - 1:0] down_data;
     logic                up_valid;
+    logic                up_ready;
+    logic                down_valid;
     logic                down_ready;
 
     // Using queues because iverilog doesn't support mailboxes
-    logic [W_FIFO - 1:0] model_fifo [$];
-    logic [W_FIFO - 1:0] dut_fifo   [$];
+    logic [W_FIFO - 1:0] fifo_in  [$];
+    logic [W_FIFO - 1:0] fifo_out [$];
 
     fifo
     # (
@@ -30,6 +34,8 @@ module fifo_tb();
         .rst        ( rst        ),
         .up_data    ( up_data    ),
         .up_valid   ( up_valid   ),
+        .up_ready   ( up_ready   ),
+        .down_valid ( down_valid ),
         .down_ready ( down_ready ),
         .down_data  ( down_data  )
     );
@@ -42,15 +48,15 @@ module fifo_tb();
     end
 
     initial begin
-        dump();
-        reset();
-        init();
+        dump  ();
+        reset ();
+        init  ();
 
         fork
-            drive_in (DELAY_MIN, DELAY_MAX);
-            drive_out(DELAY_MIN, DELAY_MAX);
-            monitor();
-            scoreboard();
+            drive_in   (PUSH_DELAY_MIN, PUSH_DELAY_MAX);
+            drive_out  (POP_DELAY_MIN, POP_DELAY_MAX);
+            monitor    ();
+            scoreboard ();
         join
     end
 
@@ -58,78 +64,89 @@ module fifo_tb();
         down_ready <= 0;
         up_valid   <= 0;
 
-        dut_fifo    = {};
-        model_fifo  = {};
+        fifo_in     = {};
+        fifo_out    = {};
     endtask
 
     task drive_in(int delay_min, int delay_max);
         int delay;
+
         forever begin
             delay = $urandom_range(delay_min, delay_max);
             repeat (delay) @(posedge clk);
 
             up_valid <= 1;
             up_data  <= $urandom();
-            @(posedge clk);
+
+            do begin
+                @(posedge clk);
+            end
+            while (~up_ready);
+
             up_valid <= 0;
         end
     endtask
 
     task drive_out(int delay_min, int delay_max);
         int delay;
+
         forever begin
             delay = $urandom_range(delay_min, delay_max);
             repeat (delay) @(posedge clk);
 
             down_ready <= 1;
-            @(posedge clk);
+
+            do begin
+                @(posedge clk);
+            end
+            while (~down_valid);
+
             down_ready <= 0;
         end
     endtask
 
     task monitor();
-        int model_size = 0;
-
         forever begin
             @(posedge clk);
-            if (up_valid & model_size < 2 ** D_FIFO) begin
-                model_fifo.push_back(up_data);
-                model_size++;
-            end
 
-            if (down_ready & model_size > 0) begin
-                dut_fifo.push_back(down_data);
-                model_size--;
-            end
+            if (up_valid & up_ready)
+                fifo_in.push_back(up_data);
+
+            if (down_ready & down_valid)
+                fifo_out.push_back(down_data);
         end
     endtask
 
     task scoreboard();
-        logic [W_FIFO - 1:0] fifo_in, fifo_out;
+        logic [W_FIFO - 1:0] data_in;
+        logic [W_FIFO - 1:0] data_out;
+
         repeat(CHECKS) begin
+            do begin
+                @(posedge clk);
+            end
+            while (fifo_in.size() == 0);
+
+            data_in = fifo_in[0];
+            fifo_in.delete(0);
 
             do begin
                 @(posedge clk);
             end
-            while (model_fifo.size() == 0);
+            while (fifo_out.size() == 0);
 
-            fifo_in = model_fifo[0];
-            model_fifo.delete(0);
+            data_out = fifo_out[0];
+            fifo_out.delete(0);
 
-            do begin
-                @(posedge clk);
-            end
-            while (dut_fifo.size() == 0);
-
-            fifo_out = dut_fifo[0];
-            dut_fifo.delete(0);
-
-            if (fifo_in !== fifo_out) begin
-                $display("Error at time %t, in: %d, out: %d",
-                          $time, fifo_in, fifo_out);
+            if (data_in !== data_out) begin
+                $error("Error at time %t, in: %d, out: %d",
+                          $time, data_in, data_out);
                 $finish();
             end
         end
+        $display("-----------------------------------");
+        $display("All checks completed with no errors");
+        $display("-----------------------------------");
         $finish();
     endtask
 
