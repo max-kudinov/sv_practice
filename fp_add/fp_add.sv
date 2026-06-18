9ab79bede1be950c6ce3f910e62bd965630f56c4
Maxim Kudinov <max.kudinov@proton.me>
2026-06-19 02:26:10 +0300

    Fix IEEE 754 implementation errors

--- fp_add/fp_add.sv
+++ fp_add/fp_add.sv
@@ -6,44 +6,56 @@ module fp_add (
     output var logic [31:0] sum_o
 );
 
-logic sign_1;
-logic sign_2;
+localparam int unsigned W_MANT     = 23;
+localparam int unsigned W_MANT_EXT = W_MANT + 1;
+localparam int unsigned W_EXP      = 8;
+localparam int unsigned W_MANT_IDX = $clog2(W_MANT_EXT + 1);
 
-logic [7:0] exponent_1;
-logic [7:0] exponent_2;
+logic                  sign_1;
+logic                  sign_2;
 
-logic [22:0] mantissa_1;
-logic [22:0] mantissa_2;
+logic [W_EXP-1:0]      exponent_1;
+logic [W_EXP-1:0]      exponent_2;
 
-logic [23:0] mantissa_ext_1;
-logic [23:0] mantissa_ext_2;
+logic [W_MANT-1:0]     mantissa_1;
+logic [W_MANT-1:0]     mantissa_2;
 
-logic [23:0] mantissa_aligned_1;
-logic [23:0] mantissa_aligned_2;
+logic [W_MANT_EXT-1:0] mantissa_ext_1;
+logic [W_MANT_EXT-1:0] mantissa_ext_2;
 
-logic [23:0] add_result;
-logic [22:0] mantissa_normalized;
+logic [W_MANT_EXT-1:0] mantissa_aligned_1;
+logic [W_MANT_EXT-1:0] mantissa_aligned_2;
 
-logic [7:0]  exponent_diff;
-logic        shift_align_num;
+logic [W_MANT_EXT-1:0] add_result;
+logic [W_MANT-1:0]     mantissa_normalized;
 
-logic res_sign;
-logic [7:0] res_exponent;
-logic overflow;
+logic [W_EXP-1:0]      exponent_diff;
+logic                  shift_align_num;
 
-logic [23:0] pri_onehot;
-logic        prev_ones;
-logic [23:0] conv_table [5];
-logic [4:0]  normalize_shamt;
+logic                  res_sign;
+logic [W_EXP-1:0]      res_exponent;
+logic                  overflow;
+
+logic [W_MANT_EXT-1:0] pri_onehot;
+logic                  prev_ones;
+logic [W_MANT_EXT-1:0] conv_table [W_MANT_IDX];
+logic [W_MANT_IDX-1:0] normalize_shamt;
 
 always_comb begin
     // Decode
     { sign_1, exponent_1, mantissa_1 } = num_1_i;
     { sign_2, exponent_2, mantissa_2 } = num_2_i;
 
-    // Prepend leading 1
-    mantissa_ext_1 = { 1'b1, mantissa_1 };
-    mantissa_ext_2 = { 1'b1, mantissa_2 };
+    // Prepend leading 1 if the number is not 0
+    if (exponent_1 != '0)
+        mantissa_ext_1 = { 1'b1, mantissa_1 };
+    else
+        mantissa_ext_1 = { 1'b0, mantissa_1 };
+
+    if (exponent_2 != '0)
+        mantissa_ext_2 = { 1'b1, mantissa_2 };
+    else
+        mantissa_ext_2 = { 1'b0, mantissa_2 };
 
     // Compare exponents
     if (exponent_1 > exponent_2) begin
@@ -69,34 +81,35 @@ always_comb begin
 
         // Subtract negative from positive
         if (sign_1)
-            add_result = mantissa_aligned_2 - mantissa_aligned_1;
+            { res_sign, add_result } = mantissa_aligned_2 - mantissa_aligned_1;
         else
-            add_result = mantissa_aligned_1 - mantissa_aligned_2;
+            { res_sign, add_result } = mantissa_aligned_1 - mantissa_aligned_2;
 
-        res_sign = add_result[23];
+        // If the result is negative, take an absolute value
+        if (res_sign)
+            add_result = -add_result;
 
     end else begin
 
         { overflow, add_result } = mantissa_aligned_1 + mantissa_aligned_2;
-
         res_sign = sign_1;
 
     end
 
-    for (int i = 23; i >= 0; i--) begin
+    for (int i = W_MANT_EXT-1; i >= 0; i--) begin
         prev_ones = '0;
 
-        for (int j = 23; j > i; j--)
+        for (int j = W_MANT_EXT-1; j > i; j--)
             prev_ones |= add_result[j];
 
         pri_onehot[i] = add_result[i] && !prev_ones;
     end
 
-    for (int unsigned i = 0; i < 32; i++)
-        for (int unsigned j = 0; j < 5; j++)
-            conv_table[j][31-i] = logic'((i >> j));
+    for (int unsigned i = 0; i < W_MANT_EXT; i++)
+        for (int unsigned j = 0; j < W_MANT_IDX; j++)
+            conv_table[j][W_MANT_EXT-1-i] = logic'((i >> j));
 
-    for (int unsigned i = 0; i < 5; i++)
+    for (int unsigned i = 0; i < W_MANT_EXT; i++)
             normalize_shamt[i] = |(conv_table[i] & pri_onehot);
 
     // Adjust exponent
@@ -107,7 +120,7 @@ always_comb begin
         else
             res_exponent = exponent_1 + 1'b1;
 
-        mantissa_normalized = 23'(add_result);
+        mantissa_normalized = W_MANT'(add_result >> 1);
 
     end else begin
 
@@ -116,7 +129,7 @@ always_comb begin
         else
             res_exponent = exponent_1 - 8'(normalize_shamt);
 
-        mantissa_normalized = 23'(add_result << normalize_shamt);
+        mantissa_normalized = W_MANT'(add_result << normalize_shamt);
 
     end
 
--- fp_add/tb.sv
+++ fp_add/tb.sv
@@ -11,12 +11,43 @@ fp_add fp_add (
 );
 
 initial begin
-    num_1 = $shortrealtobits(0.69);
-    num_2 = $shortrealtobits(0.400);
-    #1;
-    $display("%f", $bitstoshortreal(sum));
-    $display("%f", 0.69 + 0.400);
-    $display("%b", sum);
+    shortreal a;
+    shortreal b;
+    shortreal expected;
+    $dumpvars;
+
+    for (int i = 0; i < 100; i++) begin
+        a = $bitstoshortreal($urandom);
+        b = $bitstoshortreal($urandom);
+        num_1 = $shortrealtobits(a);
+        num_2 = $shortrealtobits(b);
+        #1;
+
+        expected = a + b;
+
+        if (sum !== $shortrealtobits(expected)) begin
+            $display("Test: %d", i);
+            $display("a: %f", a);
+            $display("b: %f", b);
+            $display("Actual: %f", $bitstoshortreal(sum));
+            $display("Expected: %f", expected);
+            $display("Actual: %b", sum);
+            $display("Expected: %b\n", $shortrealtobits(expected));
+            $finish;
+        end
+
+        // $display("%b", sum);
+        // $display("%h", sum);
+        // $display("My exponent is: %b", sum[30-:8]);
+        // $display("My mantissa is: %b", sum[22:0]);
+        // $display("a: %b", num_1);
+        // $display("b: %b", num_2);
+        // $display("exp: %d", 00010010101111010000001);
+        // $display("mant: %d", 8'b00100100);
+    end
+
+    $display("All done, no mistakes hehe");
+    $finish;
 end
 
 endmodule
