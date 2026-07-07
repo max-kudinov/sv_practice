4cc717a5bac51dcbfe1b9494e5896a9e668e18a3
Maxim Kudinov <max.kudinov@proton.me>
2026-07-07 23:57:47 +0300

    Don't randomize subnormal operands for multiplication

--- floating_point/tb.sv
+++ floating_point/tb.sv
@@ -58,8 +58,18 @@ task driver;
 
         valid_i <= '1;
 
-        // Don't randomize inf and NaN, as well as negative numbers
         if (std::randomize(rand_args) with {
+            // Multiplication with subnormals is not supported
+            // because it's a pain
+            `ifdef MULT
+            if (rand_args.a[30:23] == '0)
+                rand_args.a[22:0] == '0;
+
+            if (rand_args.b[30:23] == '0)
+                rand_args.b[22:0] == '0;
+            `endif
+
+            // Don't randomize inf and NaN, as well as negative numbers
             rand_args.a[31] == '0;
             rand_args.a[30:23] != '1;
             rand_args.b[31] == '0;
